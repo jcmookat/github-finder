f25d40f688467140adc03ecc8dfde0bc14d5818e
jcmookat <jscardenas717@rocketmail.com>
2022-01-20 14:27:15 +0900

    update

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -3,7 +3,7 @@ import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa'
 import { Link, useParams } from 'react-router-dom'
 import Spinner from '../components/layout/Spinner'
 import RepoList from '../components/repos/RepoList'
-import { getUser, getUserRepos } from '../context/github/GithubActions'
+import { getUserAndRepos } from '../context/github/GithubActions'
 import GithubContext from '../context/github/GithubContext'
 
 function User() {
@@ -14,11 +14,8 @@ function User() {
 	useEffect(() => {
 		dispatch({ type: 'SET_LOADING' })
 		const getUserData = async () => {
-			const userData = await getUser(params.login)
-			dispatch({ type: 'GET_USER', payload: userData })
-
-			const userRepoData = await getUserRepos(params.login)
-			dispatch({ type: 'GET_REPOS', payload: userRepoData })
+			const userData = await getUserAndRepos(params.login)
+			dispatch({ type: 'GET_USER_AND_REPOS', payload: userData })
 		}
 		getUserData()
 	}, [dispatch, params.login])
